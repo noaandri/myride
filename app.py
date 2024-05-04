ccc61e34660f50c84f58bf504c09d22fd8822428
noa <noa.roth@stud.fhgr.ch>
2024-05-04 10:14:40 +0200

    rebuild architecture

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("Hello World")
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name="Noa Roth"
+    return render_template("index.html", name=name)
+
+@app.route('/page1')
+def hi():
+    name="Page1"
+    return render_template("Page1.html", name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
