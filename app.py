95d25eac5e570bbedd4df09c67e44adf09073640
noa <noa.roth@stud.fhgr.ch>
2024-05-05 21:55:42 +0200

    try to fix

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
- from flask import Flask, render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    name="Noa Roth"
+def home():
+    ##name="Noa Roth"
     return render_template("index.html", name=name)
 
 @app.route('/page1')
 def hi():
-    name="Page1"
+    ##name="Page1"
     return render_template("Page1.html", name=name)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
